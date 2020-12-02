e9a55493f6b8d17ee49a915a5dcad0d0592aa8cf
bea73693 <bea73693@bethel.edu>
2020-12-01 20:33:58 -0600

    add page for each phase

--- Echidna/public/js/page5.js
+++ Echidna/public/js/page5.js
@@ -1,3 +1,17 @@
 window.onload = () => {
+    document.getElementById('button').onclick = courses;
     console.log('Page 5 boi');
+}
+
+async function courses() {
+    fetch('/action/page5', { method: 'POST' }).then(async response => {
+        if (!response.ok) {
+            let responseMessage = await response.text();
+            document.getElementById('result').innerHTML = responseMessage;
+            return;
+        }
+        let responseJson = await response.json();
+        document.getElementById('result').innerHTML = '';
+        constructTable('#result', responseJson);
+    });
 }
