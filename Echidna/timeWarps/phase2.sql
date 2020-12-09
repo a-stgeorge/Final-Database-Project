55c23673c3dfaa990c17d6f7e4d7416680708ffb
Joshua Halverson <joh73333@bethel.edu>
2020-12-09 14:41:52 -0600

    Update phase2.sql

--- Echidna/timeWarps/phase2.sql
+++ Echidna/timeWarps/phase2.sql
@@ -13,36 +13,37 @@ insert into instructor values(10000010, 'Sally', 'Smith', 'ssmith@college.edu', 
 
 /* COURSES */
 
-insert into course values('COS100', 'Intro to Programming', 'MathCs', 3, NULL);
-insert into course values('COS105', 'Object-oriented Design and Programming', 'MathCs', 4, NULL);
-insert into course values('COS205', 'Scientific Computing', 'MathCs', 3, NULL);
-insert into course values('COS212', 'Data Structures', 'MathCs', 4, NULL);
-insert into course values('COS214', 'Computer Architecture', 'MathCs', 4, NULL);
-insert into course values('COS216', 'Algorithms and Advanced Data Structures', 'MathCs', 3, NULL);
-insert into course values('COS235', 'Computer Systems', 'MathCs', 4, NULL);
-insert into course values('COS301', 'Operating Systems', 'MathCs', 4, NULL);
-insert into course values('COS313', 'Database Systems', 'MathCs', 3, NULL);
-insert into course values('COS318', 'Web Programming', 'MathCs', 3, NULL);
-insert into course values('COS320', 'Computer Graphics Programming', 'MathCs', 3, NULL);
-insert into course values('COS334', 'Data Mining and Machine Learning', 'MathCs', 3, NULL);
-insert into course values('COS335', 'Computer Security', 'MathCs', 3, NULL);
-insert into course values('COS351', 'High Performance Computing', 'MathCs', 3, NULL);
-insert into course values('COS371', 'Organization of Programming Languages', 'MathCs', 3, NULL);
-insert into course values('MAT101', 'Math for the 21st Century', 'MathCs', 3, NULL);
-insert into course values('MAT102', 'Creative Problem Solving', 'MathCs', 3, NULL);
-insert into course values('MAT123', 'Precalculus', 'MathCs', 3, NULL);
-insert into course values('MAT124', 'Calculus 1', 'MathCs', 3, NULL);
-insert into course values('MAT125', 'Calculus 2', 'MathCs', 3, NULL);
-insert into course values('MAT201', 'Mathematics for Elementary Education 1', 'MathCs', 3, NULL);
-insert into course values('MAT202', 'Mathematics for Elementary Education 2', 'MathCs', 3, NULL);
-insert into course values('MAT207', 'Statistical Analysis', 'MathCs', 3, NULL);
-insert into course values('MAT209', 'Financial Mathematics', 'MathCs', 3, NULL);
-insert into course values('MAT211', 'Linear Algebra', 'MathCs', 3, NULL);
-insert into course values('MAT222', 'Differential Equations', 'MathCs', 3, NULL);
-insert into course values('MAT223', 'Multivariable Calculus', 'MathCs', 3, NULL);
-insert into course values('MAT241', 'Discrete Mathematics', 'MathCs', 3, NULL);
-insert into course values('MAT330', 'Probability and Statistics', 'MathCs', 3, NULL);
-insert into course values('MAT376', 'Operations Research', 'MathCs', 3, NULL);
-insert into course values('MAT422', 'Real Analysis', 'MathCs', 4, NULL);
-insert into course values('MAT499', 'Foundations of Mathematics', 'MathCs', 3, NULL);
-insert into course values('NIL000', 'Non-Instructional Load', 'General', 0, NULL);          /* Non instructional loads now are 0 credits */
+
+insert into course values('COS100', 'Intro to Programming', 'MathCs', 3);
+insert into course values('COS105', 'Object-oriented Design and Programming', 'MathCs', 4);
+insert into course values('COS205', 'Scientific Computing', 'MathCs', 3);
+insert into course values('COS212', 'Data Structures', 'MathCs', 4);
+insert into course values('COS214', 'Computer Architecture', 'MathCs', 4);
+insert into course values('COS216', 'Algorithms and Advanced Data Structures', 'MathCs', 3);
+insert into course values('COS235', 'Computer Systems', 'MathCs', 4);
+insert into course values('COS301', 'Operating Systems', 'MathCs', 4);
+insert into course values('COS313', 'Database Systems', 'MathCs', 3);
+insert into course values('COS318', 'Web Programming', 'MathCs', 3);
+insert into course values('COS320', 'Computer Graphics Programming', 'MathCs', 3);
+insert into course values('COS334', 'Data Mining and Machine Learning', 'MathCs', 3);
+insert into course values('COS335', 'Computer Security', 'MathCs', 3);
+insert into course values('COS351', 'High Performance Computing', 'MathCs', 3);
+insert into course values('COS371', 'Organization of Programming Languages', 'MathCs', 3);
+insert into course values('MAT101', 'Math for the 21st Century', 'MathCs', 3);
+insert into course values('MAT102', 'Creative Problem Solving', 'MathCs', 3);
+insert into course values('MAT123', 'Precalculus', 'MathCs', 3);
+insert into course values('MAT124', 'Calculus 1', 'MathCs', 3);
+insert into course values('MAT125', 'Calculus 2', 'MathCs', 3);
+insert into course values('MAT201', 'Mathematics for Elementary Education 1', 'MathCs', 3);
+insert into course values('MAT202', 'Mathematics for Elementary Education 2', 'MathCs', 3);
+insert into course values('MAT207', 'Statistical Analysis', 'MathCs', 3);
+insert into course values('MAT209', 'Financial Mathematics', 'MathCs', 3);
+insert into course values('MAT211', 'Linear Algebra', 'MathCs', 3);
+insert into course values('MAT222', 'Differential Equations', 'MathCs', 3);
+insert into course values('MAT223', 'Multivariable Calculus', 'MathCs', 3);
+insert into course values('MAT241', 'Discrete Mathematics', 'MathCs', 3);
+insert into course values('MAT330', 'Probability and Statistics', 'MathCs', 3);
+insert into course values('MAT376', 'Operations Research', 'MathCs', 3);
+insert into course values('MAT422', 'Real Analysis', 'MathCs', 4);
+insert into course values('MAT499', 'Foundations of Mathematics', 'MathCs', 3);
+insert into course values('NIL000', 'Non-Instructional Load', 'General', 0);  /* Non instructional loads now are 0 credits */
